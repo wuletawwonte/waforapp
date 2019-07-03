2f26f7b39117d072611ae434d7d190ed04e7932c
Wuletaw <wuletawwonte@yahooo.com>
2019-07-03 10:18:15 +0300

    to blen

--- application/config/database.php
+++ application/config/database.php
@@ -50,9 +50,9 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'toor';
+$db['default']['password'] = '';
 $db['default']['database'] = 'waforapp';
-$db['default']['dbdriver'] = 'mysql';
+$db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
